9d5ba28cbb47e215b0e271899c00cf7a046540cc
ColinFChevalier <colinfchevalier@gmail.com>
2021-09-14 14:39:39 -0500

    framework for pulling track data from api

--- src/components/Play/PlayDetail.js
+++ src/components/Play/PlayDetail.js
@@ -1,33 +1,44 @@
 import React, { useState, createContext, useContext } from "react"
-import { useHistory } from "react-router"
+import { useHistory, useParams } from "react-router"
 import { PlayContext } from "./PlayProvider"
 
 
 export const embedPlayer = () => {
 
     const { tracks, getTracks, moods, getMoods } = useContext(PlayContext)
+    const [track, setTrack] = useState({ track: {}, mood: {}})
 
     const history = useHistory()
-
+    const { trackId } = useParams()
     useEffect(() => {
-        console.log("CustomerList: useEffect - getCustomers")
+        console.log("PlayDetail: useEffect - getTracks")
         getTracks()
     }, [])
 
     useEffect(() => {
-        console.log("CustomerList: useEffect - getCustomers")
+        console.log("PlayDetail: useEffect - getMoods")
         getMoods()
     }, [])
 
+    useEffect(() => {
+        if (props.track) {
+            setTrack(props.track)
+        } else {
+            const thisTrack = tracks.find(a => a.id === parseInt(trackId)) || { location: {}, customer: {} }
+
+            setTrack(thisTrack)
+        }
+    }, [animalId])
+    
     return (
         <div class="ui two column centered grid">
             <div class="column">
                 <section className="Tracks">
-                    <h2 align='center'>{ mood.name }</h2>
+                    <h2 align='center'>{ moods.name }</h2>
                     <div className="embed" >
                         
                         <iframe className="embed_player"
-                            src="https://bandcamp.com/EmbeddedPlayer/album=3426299746/size=large/bgcol=333333/linkcol=4ec5ec/tracklist=false/track=3125070522/transparent=true/"
+                            src={track.embedPlayerSRC}
                             seamless><a href="https://oralsax.bandcamp.com/album/type-zero-2">Type Zero by Oral Sax</a>
                         </iframe>
                     </div>
