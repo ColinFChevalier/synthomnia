57baad74dd37beec6ee3f561ef9e617d28622e3e
ColinFChevalier <colinfchevalier@gmail.com>
2021-09-24 17:17:50 -0500

    AboutForm displaying

--- src/components/About/AboutForm.js
+++ src/components/About/AboutForm.js
@@ -1,43 +1,54 @@
 import React, { Component } from 'react'
-import { Input, Menu, Segment } from 'semantic-ui-react'
+import { Grid, Menu, Segment, Button } from 'semantic-ui-react'
 
-export const AboutForm = () =>  {
-  state = { activeItem: 'home' }
+export class AboutForm extends Component {
+  state = { activeItem: 'github' }
 
   handleItemClick = (e, { name }) => this.setState({ activeItem: name })
 
-   {
+  render() {
     const { activeItem } = this.state
 
     return (
-      <div>
-        <Menu pointing>
-          <Menu.Item
-            name='home'
-            active={activeItem === 'home'}
-            onClick={this.handleItemClick}
-          />
-          <Menu.Item
-            name='messages'
-            active={activeItem === 'messages'}
-            onClick={this.handleItemClick}
-          />
-          <Menu.Item
-            name='friends'
-            active={activeItem === 'friends'}
-            onClick={this.handleItemClick}
-          />
-          <Menu.Menu position='right'>
-            <Menu.Item>
-              <Input icon='search' placeholder='Search...' />
-            </Menu.Item>
-          </Menu.Menu>
-        </Menu>
+      <Grid>
+        <Grid.Column width={4}>
+          <Menu fluid vertical tabular>
+            <Menu.Item
+              name='github'
+              active={activeItem === 'github'}
+              onClick={this.handleItemClick}
+            />
+            <Menu.Item
+              name='pics'
+              active={activeItem === 'pics'}
+              onClick={this.handleItemClick}
+            />
+            <Menu.Item
+              name='companies'
+              active={activeItem === 'companies'}
+              onClick={this.handleItemClick}
+            />
+            <Menu.Item
+              name='links'
+              active={activeItem === 'links'}
+              onClick={this.handleItemClick}
+            />
+          </Menu>
+        </Grid.Column>
 
-        <Segment>
-          <img src='/images/wireframe/paragraph.png' />
-        </Segment>
-      </div>
+        <Grid.Column stretched width={12}>
+          <Segment>
+            <Button.Group vertical>
+    <Button>Feed</Button>
+    <Button>Messages</Button>
+    <Button>Events</Button>
+    <Button>Photos</Button>
+  </Button.Group>
+          </Segment>
+        </Grid.Column>
+      </Grid>
     )
   }
 }
+
+export default AboutForm
